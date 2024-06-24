6286254354a29138813922b54b4c79ebe268d4fa
leerlandais <leerlandais@gmail.com>
2024-06-24 13:16:43 +0200

    prep for later

--- Exercices/Lee/scripts/todo.js
+++ Exercices/Lee/scripts/todo.js
@@ -1,23 +1,33 @@
 const   listContainer = document.getElementById("listContainer"),
-        noListMessage = document.getElementById("noListMessage");
+        noListMessage = document.getElementById("noListMessage"),
+        addNewButton  = document.getElementById("addNewButton");
 
+        //récuperation de totale items sur la liste et crétion d'un array pour les stocké
 let listCount = localStorage.getItem("listCount"),
     listArray = [];
 
+    // Si items existe, ajoute dans array
 for (let i = 0; i < listCount; i++) {
     listArray.push(localStorage.getItem("listItem"+(i+1)));
 }
-console.log(listArray);
 
+// Si liste est vide, affiche un message, sinon appel le fonction de création liste
 listCount == 0 ? noListMessage.textContent = "La Liste Est Vide"
              : createList(listArray);
 
+
 function createList(list){
-    const UL = document.createElement("ul"); 
+    // création d'un UL pour stocké la liste
+    const UL = document.createElement("ul");
+
     list.forEach(function(item){
+        // pour chaque listItem, créé un LI
             let LI = document.createElement("li");
+            // et le remplir
             LI.textContent = item;
+            // et ajoute LI dans UL
             UL.appendChild(LI);       
         });
+        //et finalement, ajoute UL dans le div préparé pour l'affichage
     listContainer.appendChild(UL);
 }
