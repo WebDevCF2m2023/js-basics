467b7dda2c61b1de4b9ee90eb1cc4fad756f2e79
Tevin01 <tevin.eliya.bofona@cf2000.onmicrosoft.com>
2024-06-24 12:06:47 +0200

    feels like throwinh thyselft in a trash can

---
+++ Tevin/Switch.js
@@ -0,0 +1,31 @@
+let score = "";
+
+switch(score){
+    case "A":
+        console.log("Excellent");
+        break;
+
+    case "B":
+        console.log("Très bien");
+        break;
+
+    case "C":
+        console.log("Bien");
+        break;
+        
+    case "D":
+        console.log("Satisfaisant");
+        break;
+
+    case "E":
+        console.log("Peut mieux faire");
+        break;
+
+    case "F":
+        console.log("Echec");
+        break;
+
+    case "PAs":
+        console.log("Dans la salle d'entrainement, tout de suite !");
+        break;
+}
