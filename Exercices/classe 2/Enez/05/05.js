a9dc6ea7a901d4f5fb7e1025d591d8b328dd1a02
EEnez <enezgubeljic@gmail.com>
2024-06-27 13:31:30 +0200

    exo

---
+++ Exercices/classe 2/Enez/05/05.js
@@ -0,0 +1,22 @@
+function multiplyByFive(number) {
+    return number * 5;
+} 
+
+console.log(multiplyByFive(5)); // 25
+console.log(multiplyByFive(10)); // 50
+console.log(multiplyByFive(15)); // 75
+console.log(multiplyByFive(20)); // 100
+console.log(multiplyByFive(25)); // 125
+console.log(multiplyByFive(30)); // 150
+console.log(multiplyByFive(35)); // 175
+console.log(multiplyByFive(40)); // 200
+
+
+function multiplyByNumber(number, multiplier) {
+    return number * multiplier;
+}
+
+console.log(multiplyByNumber(5, 5)); // 25
+console.log(multiplyByNumber(10, 5)); // 50
+console.log(multiplyByNumber(15, 5)); // 75
+console.log(multiplyByNumber(20, 5)); // 100
