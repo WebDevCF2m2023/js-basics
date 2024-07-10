94b8fd3ca221565fcbaeeb4809a9096e1938406b
leerlandais <leerlandais@gmail.com>
2024-07-10 14:21:36 +0200

    links added

---
+++ Exercices/Lee-TWversions/scripts/index.js
@@ -0,0 +1,5 @@
+linkButton = document.querySelectorAll(".linkButton");
+
+for (let i = 0; i < linkButton.length; i++) {
+    linkButton[i].className = "bg-gradient-to-b w-max mx-auto my-12 text-blue-500 font-semibold from-slate-50 to-blue-100 px-10 py-3 rounded-2xl shadow-blue-600 shadow-xl border-b-4 hover border-b border-blue-200 hover:shadow-sm transition-all duration-500"
+}
