2b51c9a984c576d709e29bb76fdab0f35c4cd6d7
erdaserhan <erhan209@gmail.com>
2024-08-29 10:05:25 +0200

    update

--- Exercices/Classe1/Erhan/ex15/script.js
+++ Exercices/Classe1/Erhan/ex15/script.js
@@ -7,24 +7,23 @@ const storageKey = "items"
 function renderItems() {
     itemsDiv.innerHTML = null; 
 
-    //The Object.entries() static method returns an array of a given object's own enumerable string-keyed property key-value pairs.
+    //The Object.entries() static method returns an array of a given object's own enumerable string-keyed property key-value pairs.   
     for (const [idx, item] of Object.entries(items)){
         const container = document.createElement("div")//We create new div container with DOM.
         container.style.marginBottom = "10px"
 
         const text = document.createElement("p")
         text.style.display = "inline"
-        text.style.marginRight = "20px"
+        text.style.marginRight = "15vw"
         text.textContent = item;
 
         const button = document.createElement("button")
         button.textContent = "Delete";
-        button.style.border = "2px solid tomato";
+        button.style.border = "2px solid lightgrey";
         button.onclick = () => removeItem(idx) // ()=> call this function in another function. Otherwise it is gonna call the removeItem function automatically.
 
         container.appendChild(text)
         container.appendChild(button)
-
         itemsDiv.appendChild(container)
     }
 }
@@ -46,8 +45,10 @@ function addItem() {
         alert("You cannot add an empty item")
         return
     }
+    
     items.push(value)
     renderItems()
+    alert("Added to your list")
     input.value = ""
     saveItems()
 }
